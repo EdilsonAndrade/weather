f5bd665dcd33bc81cd4a23ba336ca2a3abc773ea
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-12 16:01:32 -0300

    refactor: refactor initial code from the scafolding react

---
+++ weather_web/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { WeatherView } from "./pages/WeatherView";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherView />
+    </div>
+  );
+}
+
+export default App;
